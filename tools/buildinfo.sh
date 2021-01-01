PixelExperience/build

build: Use CUSTOM_BUILD for OTA prop
Change-Id: I3e417ff98af75927a1f366616363b49d5c356339

tools/buildinfo.sh
@@ -61,6 +61,6 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
   echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
 fi
 
-echo "org.pixelexperience.device=$CUSTOM_DEVICE"
+echo "org.pixelexperience.device=$CUSTOM_BUILD"
 
 echo "# end build properties"

RohieOS/platform_build_make

tools/buildinfo.sh
@@ -52,6 +52,6 @@ if [ -n "$BUILD_THUMBPRINT" ] ; then
   echo "ro.build.thumbprint=$BUILD_THUMBPRINT"
 fi
 
-echo "ro.rohie.device=$CUSTOM_DEVICE"
+echo "ro.rohie.device=$CUSTOM_BUILD"
 
 echo "# end build properties"